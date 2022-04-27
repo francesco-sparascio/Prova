03fd53d7d6a8409d78f67887ca014397db51519a
franc <franc@DESKTOP-L4PMMOB>
2022-04-27 12:32:44 +0200

    versione 1

---
+++ src/it/meucci/sparascio/Main.java
@@ -0,0 +1,15 @@
+package it.meucci.sparascio;
+/**
+ * 
+ * @author Sparascio Francesco
+ * @version 1.0
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Prova GitHub versione 1");
+
+	}
+
+}
