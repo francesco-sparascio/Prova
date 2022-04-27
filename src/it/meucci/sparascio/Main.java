c7f20300c70589e2c437000b4ce2e9f91fd00219
franc <franc@DESKTOP-L4PMMOB>
2022-04-27 12:33:51 +0200

    versione 2

--- src/it/meucci/sparascio/Main.java
+++ src/it/meucci/sparascio/Main.java
@@ -2,13 +2,13 @@ package it.meucci.sparascio;
 /**
  * 
  * @author Sparascio Francesco
- * @version 1.0
+ * @version 2.0
  */
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Prova GitHub versione 1");
+		System.out.println("Prova GitHub versione 2");
 
 	}
 
